2cb4d558a4353da6068f2d088e5677cfcd8750f4
Martijn Snijder <martijn-snijder@hotmail.com>
2016-05-17 12:59:26 +0200

    Added custom camera

--- app/src/main/java/snijder/martijn/photohunt/CameraFragment.java
+++ app/src/main/java/snijder/martijn/photohunt/CameraFragment.java
@@ -4,23 +4,16 @@ package snijder.martijn.photohunt;
  * Created by SnijderMMartijn on 17-5-2016.
  */
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -29,10 +22,10 @@ public class CameraFragment extends Fragment implements TextureView.SurfaceTextu
     private Camera mCamera;
     private TextureView mTextureView;
     private DrawerLayout mDrawer;
-    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
         mDrawer = (DrawerLayout) this.getActivity().findViewById(R.id.drawer);
         mDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
         View view = inflater.inflate(R.layout.camera_overlay, container, false);
